56cdd1063732250c23cdbdb4b57ba230eb2d1a35
Tanay <tanayj2107@gmail.com>
2025-12-26 01:50:51 -0500

    fixed app_id and app_key vars

--- scrapers/indeed_scraper.py
+++ scrapers/indeed_scraper.py
@@ -1,4 +1,19 @@
 from dotenv import load_dotenv
 import requests
+import os
 
-response = requests.get(url = https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id={YOUR_APP_ID}&app_key={YOUR_APP_KEY})
+#loaded env
+load_dotenv()
+
+
+app_id = os.getenv('ADZUNA_APP_ID')
+app_key = os.getenv('ADZUNA_API_KEY')
+
+response = requests.get(url = f"https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id={app_id}&app_key={app_key}")
+
+try:
+    response.raise_for_status()
+    response.json()
+    print("SUCCESS")
+except ValueError:
+    print("FAILED")
