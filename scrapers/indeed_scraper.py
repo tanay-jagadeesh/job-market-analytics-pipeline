df54197dc183aac1e4c58ec205b5c6754e6fc05f
Tanay <tanayj2107@gmail.com>
2025-12-27 00:11:13 -0500

    decided to use jsearch api key instead of adzuna (pricing)

--- scrapers/indeed_scraper.py
+++ scrapers/indeed_scraper.py
@@ -6,9 +6,7 @@ import pandas as pd
 #loaded env
 load_dotenv()
 
-
-app_id = os.getenv('ADZUNA_APP_ID')
-app_key = os.getenv('ADZUNA_API_KEY')
+app_key = os.getenv('JSEARCH_API_KEY')
 
 response = requests.get(url = f"https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id={app_id}&app_key={app_key}")
 
