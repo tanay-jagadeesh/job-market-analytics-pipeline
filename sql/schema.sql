ec5aa07407e7435b414589043de9443bf436c48a
Tanay <tanayj2107@gmail.com>
2025-12-28 17:48:15 -0500

    stored unique cities

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,3 +5,11 @@ CREATE TABLE companies (
     company_id INT PRIMARY KEY,
     company_name VARCHAR NOT NULL
 )
+
+-- storing unique cities
+
+CREATE TABLE locations (
+    location_id INT PRIMARY KEY,
+    city VARCHAR(100) NOT NULL,
+    province VARCHAR(100) NOT NULL
+)
