5783a41719b2dd443d80070e0b0f011c129104b9
Tanay <tanayj2107@gmail.com>
2025-12-28 17:49:55 -0500

    done the third table

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE job_market_db;
 
 -- storing unique company names 
+
 CREATE TABLE companies (
     company_id INT PRIMARY KEY,
     company_name VARCHAR NOT NULL
@@ -12,4 +13,9 @@ CREATE TABLE locations (
     location_id INT PRIMARY KEY,
     city VARCHAR(100) NOT NULL,
     province VARCHAR(100) NOT NULL
+)
+
+CREATE TABLE skills (
+    skill_id INT PRIMARY KEY,
+    skill_name VARCHAR NOT NULL
 )
