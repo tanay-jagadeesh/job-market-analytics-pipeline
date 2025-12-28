755e82e9902efb1213d8adc4d6ce70391a8de292
Tanay <tanayj2107@gmail.com>
2025-12-28 17:55:57 -0500

    created job posting table for more information (posted date whether it's remote or not etc.)

--- sql/schema.sql
+++ sql/schema.sql
@@ -15,7 +15,25 @@ CREATE TABLE locations (
     province VARCHAR(100) NOT NULL
 )
 
+-- storing unique skills
+
 CREATE TABLE skills (
     skill_id INT PRIMARY KEY,
     skill_name VARCHAR NOT NULL
+)
+
+-- job postings table (info)
+CREATE TABLE job_postings (
+    job_id INT PRIMARY KEY,
+    job_title VARCHAR NOT NULL,
+    company_id INT NOT NULL,
+    FOREIGN KEY (company_id) REFERENCES companies(company_id),
+    location_id INT NOT NULL,
+    FOREIGN KEY (location_id) REFERENCES locations(location_id),
+    salary_min INT NOT NULL,
+    salary_max INT NOT NULL,
+    posted_date DATE,
+    is_remote BOOLEAN,
+    experience_level VARCHAR,
+    job_description VARCHAR
 )
