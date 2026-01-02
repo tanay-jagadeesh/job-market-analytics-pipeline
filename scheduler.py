cd40a1150a75efdb023a269c9340fcb344517b12
Tanay <tanayj2107@gmail.com>
2026-01-02 01:25:58 -0500

    extract function complete (simple just had to copy code from jsearch_scraper.py)

--- scheduler.py
+++ scheduler.py
@@ -2,6 +2,34 @@ import schedule
 import time
 from datetime import datetime
 from analytics import run_all_queries
+import requests
+from dotenv import load_dotenv
+import os
+
+# ETL (Extract, Transform, Load)
+def fetch_jobs():
+    
+    load_dotenv()
+
+    app_key = os.getenv('JSEARCH_API_KEY')
+
+    url = "https://jsearch.p.rapidapi.com/search"
+
+    headers = {
+        "X-RapidAPI-Key": app_key,
+        "X-RapidAPI-Host": "jsearch.p.rapidapi.com"
+    }
+
+    params = {
+        "query": "data analyst",
+        "num_pages": 1,
+        "country": "ca"
+    }
+
+    response = requests.get(url = url, headers = headers, params = params)
+    response.raise_for_status()
+    data = response.json()
+    return data['data']
 
 
 def job():
