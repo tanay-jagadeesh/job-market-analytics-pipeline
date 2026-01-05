b6c5ccdddc54ccac41da6d77d65506d16b7cdd0a
Tanay <tanayj2107@gmail.com>
2026-01-05 11:29:09 -0500

    completed etl pipeline (imported stuff and then reorganized jsearcher.py essentially)

--- scheduler.py
+++ scheduler.py
@@ -5,6 +5,9 @@ from analytics import run_all_queries
 import requests
 from dotenv import load_dotenv
 import os
+from db import insert_job, insert_skill, insert_job_skills
+import pandas as pd
+import re 
 
 # ETL (Extract, Transform, Load)
 def fetch_jobs():
@@ -58,8 +61,42 @@ def process_jobs(jobs):
         if (job_title and "remote" in job_title.lower()) or (job_description and "remote" in job_description.lower()):
             print("This is a remote job")
 
+def load_to_database(jobs):
+    skills_list = ['python', 'sql', 'aws', 'java', 'tableau', 'power bi', 'excel', 'r', 'spark', 'azure']
+
+    for job in jobs:
+
+        job_title = job.get('job_title')
+        company_name = job.get('employer_name')
+        city = job.get('job_city')
+        province = job.get('job_state')
+        salary_min = int(job.get('job_min_salary', 0))  
+        salary_max = int(job.get('job_max_salary', 0))
+
+        job_id = insert_job(
+            job_title=job_title,
+            company_name=company_name,
+            city=city,
+            province=province,
+            salary_min=salary_min,
+            salary_max=salary_max,
+        )
+
+        # Link skills to job
+        job_description = job.get('job_description')
+        if job_description:
+            desc_lower = job_description.lower()
+            for skill in skills_list:
+                # Use word boundaries to match whole words only
+                if re.search(rf'\b{re.escape(skill)}\b', desc_lower):
+                    skill_id = insert_skill(skill)
+                    insert_job_skills(job_id, skill_id)
+
 
 def job():
+    jobs = fetch_jobs()
+    process_jobs(jobs)
+    load_to_database(jobs)
     run_all_queries()
 
 #ensures that it runs at 6 am everyday
