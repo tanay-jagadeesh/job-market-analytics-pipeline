71c17f8f0489d1c3f48b0324bcb064d907306ba1
Tanay <tanayj2107@gmail.com>
2026-01-05 10:41:47 -0500

    did the transform (automatically updating instead of manual like in jsearcher.py

--- scheduler.py
+++ scheduler.py
@@ -32,6 +32,31 @@ def fetch_jobs():
     return data['data']
 
 def process_jobs(jobs):
+    skills_list = ['python', 'sql', 'aws', 'java', 'tableau', 'power bi', 'excel', 'r', 'spark', 'azure']
+
+    for job in jobs:
+        job_description = job.get('job_description')
+        job_title = job.get("job_title")
+        
+        if job_description: 
+            desc_lower = job_description.lower()
+            
+            for skill in skills_list:
+                if skill.lower() in desc_lower:
+                    print(f"NEEDS {skill}")
+        
+        if job_title:
+            title_lower = job_title.lower()
+        
+            if "junior" in title_lower or "entry" in title_lower:
+                print("Entry Level")
+            elif "senior" in title_lower or "lead" in title_lower:
+                print("Senior Level")
+            else:
+                print("Mid Level")
+
+        if (job_title and "remote" in job_title.lower()) or (job_description and "remote" in job_description.lower()):
+            print("This is a remote job")
 
 
 def job():
